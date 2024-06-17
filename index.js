79ef8d39f3be0ee376dd046d8a9ec40ba002b8b9
Your Name <you@example.com>
2024-06-17 21:52:19 +1200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const robot = require('robotjs');
+
+console.log('请在 2 秒内将鼠标移到香蕉图片上');
+robot.setMouseDelay(2000);
+
+const mouse = robot.getMousePos();
+console.log(`鼠标位置: x=${mouse.x}, y=${mouse.y}`);
+
+while (true) {
+    robot.mouseClick();
+    console.log('Click banana');
+    robot.setMouseDelay(100); // 每次点击后等待1秒
+}
